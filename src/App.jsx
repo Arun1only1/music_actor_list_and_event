3c2486705bcdb63f11c56611e47240643303b8d2
Arun1only1 <arun1nly1@gmail.com>
2023-12-28 17:31:27 +0545

    actor list and event basics

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import SignupForm from "./components/SignUpForm";
+
+const App = () => {
+  return (
+    <>
+      <SignupForm />
+    </>
+  );
+};
+
+export default App;
